c649a23fd0dac0b9d0dd51bed73e0f86a8872493
azimos <azadeh0@gmail.com>
2013-04-09 20:23:37 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kittie::Application.config.secret_token = '25e42ce90092ac348c0b2804d68772a0eab0d6dcc14e329525d2c44c564a10ecede3718186dbf8f368a317ea65062aef11729df98d5193b63ecc424929f713c9'
